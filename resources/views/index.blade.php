b69909ee77d5b6572c49c419dcf5417a77315c77
EricNelson12 <ericjnelson012@gmail.com>
2017-07-03 23:43:22 -0700

    Let's go!

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section("title","Home")
+
+
+
+@section('content')
+<div class = "row">
+  <div class = "col-md-8">
+    Map goes here
+  </div>
+  <div class = "col-md-4">
+    Manager goes here
+  </div>
+</div>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Renter Melon - @yield('title')</title>
+        <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+      <div class = "jumbotron text-center">
+        <h1>Renter Melon!</h1>
+      </div>           
+      @yield('content')
+
+    </body>
+</html>
